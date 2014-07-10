d99619a80affaa62bda092d45da81b96ad2086af
justinandersun <justinandersun@gmail.com>
2014-07-09 22:04:47 -0400

    Micro-change

--- submitted.php
+++ submitted.php
@@ -1,25 +1,25 @@
 <?php
-            $email_to = "justinandersun@gmail.com";
-            $email_subject = "Message from Jolly Goat";
-            
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $message = $_POST['comments'];
+$email_to = "justinandersun@gmail.com";
+$email_subject = "Message from Jolly Goat";
 
-            
-            $email_message = "Hey Justin, \n\n";
-            
-            function clean_string($string) {
-              $bad = array("content-type","bcc:","to:","cc:","href");
-              return str_replace($bad,"",$string);
-            }
-            
-            $email_message .= "name: ".clean_string($name)."\n";
-            $email_message .= "email: ".clean_string($email)."\n";
-            $email_message .= "message: ".clean_string($message)."\n";
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['comments'];
 
-            $headers = 'From: '.$name."\r\n".
-            'Reply-To: '.$email."\r\n" .
-            'X-Mailer: PHP/'.phpversion();
-            @mail($email_to, $email_subject, $email_message, $headers);  
-        ?>
+
+$email_message = "Hey Justin, \n\n";
+
+function clean_string($string) {
+  $bad = array("content-type","bcc:","to:","cc:","href");
+  return str_replace($bad,"",$string);
+}
+
+$email_message .= "name: ".clean_string($name)."\n";
+$email_message .= "email: ".clean_string($email)."\n";
+$email_message .= "message: ".clean_string($message)."\n";
+
+$headers = 'From: '.$name."\r\n".
+'Reply-To: '.$email."\r\n" .
+'X-Mailer: PHP/'.phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
+?>
